1d625a34b46a5a097a90dcb6a0cdd2facc034d73
awkwardmachine <noib2009@gmail.com>
2025-10-14 10:38:27 +0100

    Fixed mismatching arguments list.

--- src/stdlib/functions/io/readln_function.cpp
+++ src/stdlib/functions/io/readln_function.cpp
@@ -5,19 +5,17 @@
 #include <llvm/IR/Type.h>
 
 bool ReadlnFunction::handlesCall(const std::string& functionName, size_t argCount) {
-    return functionName == "readln" && argCount == 0;  // readln takes 0 arguments
+    return functionName == "readln" && argCount == 0;
 }
 
 llvm::Value* ReadlnFunction::generateCall(CodeGen& context, AST::CallExpr& expr) {
     auto& builder = context.getBuilder();
     auto& llvmContext = context.getContext();
-    
-    // io_readln takes no arguments and returns char* (string)
+
     auto* i8Ptr = llvm::PointerType::get(llvm::Type::getInt8Ty(llvmContext), 0);
-    auto funcType = llvm::FunctionType::get(i8Ptr, false);  // No parameters, returns char*
+    auto funcType = llvm::FunctionType::get(i8Ptr, false);
     
     auto readlnFunc = FunctionInterface::ensureExternalFunction(context, "io_readln", funcType);
-   
-    // Call io_readln() - no arguments needed
+
     return builder.CreateCall(readlnFunc, {});
 }
